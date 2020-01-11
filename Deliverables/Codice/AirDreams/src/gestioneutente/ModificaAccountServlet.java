16ff51bfe4b35d2a2e0d55536d27a7764f3934d5
graziavarone <56019226+graziavarone@users.noreply.github.com>
2020-01-11 15:51:58 +0100

    Create ModificaAccountServlet.java

---
+++ Deliverables/Codice/AirDreams/src/gestioneutente/ModificaAccountServlet.java
@@ -0,0 +1,166 @@
+package gestioneutente;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import gestionecompagniaaerea.CompagniaAerea;
+import gestionecompagniaaerea.CompagniaAereaManager;
+
+/**
+ * Servlet implementation class ModificaAccountServlet
+ */
+@WebServlet(name="/ModificaAccountServlet",
+urlPatterns= {"/gestoreCompagnie/ModificaAccountServlet"})
+public class ModificaAccountServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static Logger logger= Logger.getLogger("global");
+	private String expNome="^[A-Za-z]{1,}$";
+	private String expCognome="^[A-Za-z]{1,}$";
+	private String expEmail="^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$";
+       
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ModificaAccountServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		UtenteManager manager=new UtenteManager();
+		CompagniaAereaManager compagniaAereaManager=new CompagniaAereaManager();
+		String message="";
+		
+		//prelevo i dati del form
+		String nome=request.getParameter("nome");
+		String cognome=request.getParameter("cognome");
+		String email=request.getParameter("email");
+		String emailVecchia=request.getParameter("emailVecchia");
+		
+		
+		String compagniaAerea=request.getParameter("combo");
+		System.out.println(compagniaAerea);
+		Account newAccount=new Account();
+		newAccount.setNome(nome);
+		newAccount.setCognome(cognome);
+		newAccount.setEmail(email);
+		
+	
+		Account accountVecchio=null;
+		Account a=null;
+		try {
+			accountVecchio = manager.findAccountByEmail(emailVecchia);
+	
+			a = manager.findAccountByEmail(email);
+			
+			if(compagniaAerea.equals("Nessuna")) {
+				newAccount.setCompagniaAerea(null);
+				newAccount.setRuolo(null);
+			} else {
+				newAccount.setCompagniaAerea(compagniaAereaManager.visualizzaInfoCompagniaAerea(compagniaAerea));
+				newAccount.setRuolo(Ruolo.gestoreVoli);
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		//controllo che la mail inviata da form (se è stata modificata) non sia uguale ad una già presente nel DB
+		if(email.equals(emailVecchia)) {
+			//l'utente non ha modificato l'email-> si e' sicuri che la sua è unica
+			try {
+				newAccount.setPassword(accountVecchio.getPassword());
+				
+				if(valida(nome,cognome,email)) {
+				manager.modificaAccount(emailVecchia, newAccount);
+				} else response.getWriter().write("Failed");
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			message+="aggiornamento dati eseguito";
+			request.setAttribute("message",message);
+			System.out.println("Modifica eseguita direttamente poiche' la email e' rimasta invariata");
+			request.setAttribute("utente",newAccount);
+		} else {
+			try {
+				
+				if (a!=null) { //trovato un account con la email appena inserita
+					message+="la email inserita è gia' presente, inserirne una nuova";
+					request.setAttribute("message",message);
+					request.setAttribute("utente", accountVecchio);
+					System.out.println("Non ho modificato l'utente poiche' esiste gia' la mail appena inserita");
+					response.getWriter().write("Failed");
+				} else {
+					newAccount.setPassword(accountVecchio.getPassword());
+			
+					if(valida(nome,cognome,email)) {
+						manager.modificaAccount(emailVecchia, newAccount);
+						} else response.getWriter().write("Failed");
+					message+="aggiornamento dati eseguito";
+					request.setAttribute("message",message);
+					System.out.println("Modifica eseguita poiche' la mail inserita non è gia' presente nel DB");
+					response.getWriter().write("Success");
+					request.setAttribute("utente",newAccount);
+				}
+				
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	
+		try {
+			request.setAttribute("compagnie", compagniaAereaManager.getCompagnie());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		request.getServletContext().getRequestDispatcher("/gestoreCompagnie/dettagliAccount.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	private boolean valida(String nome, String cognome, String email) {
+		boolean valido=true;
+		logger.info("Ho ricevuto "+nome);
+		
+		if (!Pattern.matches(expNome, nome)) {
+			logger.info("Nome non corrisponde");
+			valido=false;
+			System.out.print(nome);
+		}
+		if (!Pattern.matches(expCognome, cognome)) {
+			logger.info("Cognome non corrisponde");
+			valido=false;
+			System.out.print(cognome);
+		}
+	
+		
+		if (!Pattern.matches(expEmail, email)) {
+			valido=false;
+			System.out.print(email);
+		}
+	
+		
+		return valido;
+	
+	}
+
+}
