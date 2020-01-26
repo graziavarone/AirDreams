5c5103988c178f313d14c0a48845ea92d625761f
graziavarone <56019226+graziavarone@users.noreply.github.com>
2020-01-26 18:40:00 +0100

    Update Volo.java

--- Deliverables/Codice/AirDreams/src/Volo.java
+++ Deliverables/Codice/AirDreams/src/Volo.java
@@ -2,12 +2,18 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 public class Volo {
+	int id;
 	public  String aeroportoPartenza; //dovrebbe essere classe aeroporto
 	public String aeroportoArrivo;
 	
 	public Volo(String aeroportoPartenza, String aeroportoArrivo) {
 		this.aeroportoPartenza = aeroportoPartenza;
 		this.aeroportoArrivo = aeroportoArrivo;
+	}
+
+
+	public Volo() {
+		// TODO Auto-generated constructor stub
 	}
 
 
@@ -26,12 +32,28 @@ public class Volo {
 	public void setAeroportoArrivo(String aeroportoArrivo) {
 		this.aeroportoArrivo = aeroportoArrivo;
 	}
+	
+	
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 
 	@Override
 	public String toString() {
-		return "Volo [aeroportoPartenza=" + aeroportoPartenza + ", aeroportoArrivo=" + aeroportoArrivo + "]";
+		return "Volo [id=" + id + ", aeroportoPartenza=" + aeroportoPartenza + ", aeroportoArrivo=" + aeroportoArrivo
+				+ "]";
 	}
+
+
+
 
 
 	
