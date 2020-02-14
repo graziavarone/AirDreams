9285dc2d4ee89de77b634520ba2c26ac65c6cf39
graziavarone <56019226+graziavarone@users.noreply.github.com>
2020-02-14 19:15:04 +0100

    Create AeroportoManagerTest.java

---
+++ Deliverables/Codice/AirDreams/src/test/AeroportoManagerTest.java
@@ -0,0 +1,43 @@
+package test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import db.DriverManagerConnectionPool;
+import gestionevolo.Aeroporto;
+import gestionevolo.AeroportoManager;
+
+public class AeroportoManagerTest {
+	private AeroportoManager aeroportoManager=new AeroportoManager();
+	
+	@Before
+	public void setUp() throws Exception {	
+		//DbPopulator.initializeDatabase();
+		DriverManagerConnectionPool.setTest(true);
+	}
+	
+	@Test 
+	public void getAeroportyByCity() throws Exception{
+		ArrayList<Aeroporto> aeroporti=aeroportoManager.getAeroportiByCity("N");
+		
+		assertTrue(aeroporti.size()>0);
+	}
+	
+	@Test 
+	public void findAeroportoById() throws Exception{
+		Aeroporto aeroporto=aeroportoManager.findAeroportoById("NAP");
+		
+		assertNotNull(aeroporto);
+	}
+	
+	@After
+	public void tearDown() throws Exception{
+		DriverManagerConnectionPool.setTest(false);
+	}
+}
