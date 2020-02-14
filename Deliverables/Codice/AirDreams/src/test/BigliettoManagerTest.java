7aaaebe563b9218b8e2980894cdc9e75c4a1c1f3
graziavarone <56019226+graziavarone@users.noreply.github.com>
2020-02-14 19:15:11 +0100

    Create BigliettoManagerTest.java

---
+++ Deliverables/Codice/AirDreams/src/test/BigliettoManagerTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import db.DriverManagerConnectionPool;
+import gestioneordine.Biglietto;
+import gestioneordine.BigliettoManager;
+import gestioneordine.Sesso;
+import gestionevolo.Volo;
+import gestionevolo.VoloManager;
+
+public class BigliettoManagerTest {
+private BigliettoManager bigliettoManager=new BigliettoManager();
+private VoloManager voloManager=new VoloManager();
+	
+	@Before
+	public void setUp() throws Exception {	
+		//DbPopulator.initializeDatabase();
+		DriverManagerConnectionPool.setTest(true);
+	}
+	
+	@Test
+	public void aggiungiBiglietto() throws Exception {
+		Volo volo=voloManager.findByID("1");
+		Biglietto biglietto=new Biglietto("Rosaria", "Rossi", Sesso.F, 26,volo);
+		biglietto.setOrdine(1);
+		 biglietto=bigliettoManager.aggiungiBiglietto(biglietto);
+		 
+		 assertTrue(biglietto.getCodBiglietto()!=0);
+	}
+	
+	@Test
+	public void trovaBigliettiOrdine() throws Exception {
+		ArrayList<Biglietto> biglietti=bigliettoManager.trovaBigliettiOrdine(1);
+		 
+		 assertTrue(biglietti.size()>0);
+	}
+	
+	
+	
+	
+	
+	
+	@After
+	public void tearDown() throws Exception{
+		DriverManagerConnectionPool.setTest(false);
+	}
+}
