8da39a5a94b8ae3f8a3dd75ba9ce77ced92f0771
graziavarone <56019226+graziavarone@users.noreply.github.com>
2020-02-14 19:15:20 +0100

    Create CompagniaAereaManagerTest.java

---
+++ Deliverables/Codice/AirDreams/src/test/CompagniaAereaManagerTest.java
@@ -0,0 +1,76 @@
+package test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import db.DriverManagerConnectionPool;
+import gestionecarrello.CarrelloManager;
+import gestionecompagniaaerea.CompagniaAerea;
+import gestionecompagniaaerea.CompagniaAereaManager;
+import gestionevolo.Volo;
+
+public class CompagniaAereaManagerTest {
+	private CompagniaAereaManager compagniaAereaManager=new CompagniaAereaManager();
+	
+	@Before
+	public void setUp() throws Exception {	
+		DbPopulator.initializeDatabase();
+	}
+	
+	@Test
+	public void visualizzaInfoCompagniaAerea() throws Exception{
+		System.out.println("visualizzaInfoCompagniaAerea");
+		assertNotNull(compagniaAereaManager.visualizzaInfoCompagniaAerea("Ryanair"));
+
+	} 
+	
+	@Test
+	public void aggiungiCompagnia() throws Exception{
+		System.out.println("aggiungiCompagnia");
+		boolean result=compagniaAereaManager.aggiungiCompagnia(new CompagniaAerea("EasyJet","www.easyjet.com"));
+		
+		compagniaAereaManager.eliminaCompagnia("EasyJet");
+		assertTrue(result);
+	} 
+	
+	@Test
+	public void getCompagnie() throws Exception{
+		System.out.println("getCompagnie");
+		ArrayList<CompagniaAerea> compagnie=compagniaAereaManager.getCompagnie();
+		
+		assertEquals(1, compagnie.size());
+	} 
+	
+	@Test
+	public void getAllCompagnies() throws Exception{
+		System.out.println("getAllCompagnies");
+		ArrayList<CompagniaAerea> compagnie=compagniaAereaManager.getAllCompanies();
+		
+		assertEquals(1, compagnie.size());
+	} 
+	
+	@Test
+	public void eliminaCompagnia() throws Exception{
+		System.out.println("eliminaCompagnia");
+		
+		compagniaAereaManager.aggiungiCompagnia(new CompagniaAerea("EasyJet","www.easyjet.com"));
+		boolean result=compagniaAereaManager.eliminaCompagnia("EasyJet");
+		
+		assertTrue(result);
+	} 
+
+	
+	@After
+	public void tearDown() throws Exception{
+		DriverManagerConnectionPool.setTest(false);
+	}
+}
